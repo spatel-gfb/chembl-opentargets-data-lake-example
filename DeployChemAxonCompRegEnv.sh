c19fb5cc40b3a5db04d4db4458d7ad2f9f857c61
patsarth_gfb <spatel@goldfinchbio.com>
2020-04-23 15:06:44 -0400

    updated the script names and readme

--- DeployChemAxonCompRegEnv.sh
+++ DeployChemAxonCompRegEnv.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e -x
 
+# Browse through the required directory
+cd chem-axon-setup
+
 # Read Arguments
 export EnvironVarLower=$1
+export AwsProfile=$2
 
 # Install packages
 pip3 install -r requirements.txt
@@ -29,64 +33,36 @@ build_and_push_comp_reg_image(){
 
 }
 
-# Function to build assay docker image and push it to AWS ECR Repo
-build_and_push_assay_image(){
-
-    # Build the docker image locally
-    docker build --rm -t ${AccountId}.dkr.ecr.${AwsRegion}.amazonaws.com/${WorkflowEcrRepository}:${WorkflowAssayETLImageVersion} assay_etl_pipeline/
-
-    # Push the image on AWS ECR
-    docker push ${AccountId}.dkr.ecr.${AwsRegion}.amazonaws.com/${WorkflowEcrRepository}:${WorkflowAssayETLImageVersion}
-
-}
-
-#Get Account ID
-aws configure set region ${AwsRegion}
-export AccountId=$(aws sts get-caller-identity --output text --query 'Account')
-
-$(aws ecr get-login --no-include-email --region ${AwsRegion})
-
-#cdk deploy gfb-datalake-batch-stack --require-approval never
-#build_and_push_comp_reg_image
-#build_and_push_assay_image
-#cdk deploy gfb-datalake-batch-job-stack --require-approval never
-#cdk deploy gfb-datalake-secret-manager-stack --require-approval never
-#cdk deploy gfb-datalake-lambda-stack --require-approval never
-#cdk deploy gfb-datalake-glue-stack --require-approval never
-## cdk deploy gfb-datalake-s3-stack --require-approval never
-
-export AwsProfile=$2
+# Check for profile and if not passed, use default
 if [[ ${AwsProfile} == '' ]]
 then
     #Get Account ID
     aws configure set region ${AwsRegion}
     export AccountId=$(aws sts get-caller-identity --output text --query 'Account')
 
+    #ECR Login for pushing Docker Image
     $(aws ecr get-login --no-include-email --region ${AwsRegion})
 
     cdk deploy gfb-datalake-batch-stack --require-approval never
-    build_and_push_comp_reg_image
-    build_and_push_assay_image
     cdk deploy gfb-datalake-batch-job-stack --require-approval never
+    build_and_push_comp_reg_image
     cdk deploy gfb-datalake-secret-manager-stack --require-approval never
     cdk deploy gfb-datalake-lambda-stack --require-approval never
     cdk deploy gfb-datalake-glue-stack --require-approval never
-    # cdk deploy GFBDatalakeS3Stack --require-approval never
 
 else
     #Get Account ID
     aws configure set region ${AwsRegion} --profile ${AwsProfile}
     export AccountId=$(aws sts get-caller-identity --output text --query 'Account' --profile ${AwsProfile})
 
+    #ECR Login for pushing Docker Image
     $(aws ecr get-login --no-include-email --region ${AwsRegion} --profile ${AwsProfile})
 
     cdk deploy gfb-datalake-batch-stack --require-approval never --profile ${AwsProfile}
-    build_and_push_comp_reg_image
-    build_and_push_assay_image
     cdk deploy gfb-datalake-batch-job-stack --require-approval never --profile ${AwsProfile}
+    build_and_push_comp_reg_image
     cdk deploy gfb-datalake-secret-manager-stack --require-approval never --profile ${AwsProfile}
     cdk deploy gfb-datalake-lambda-stack --require-approval never --profile ${AwsProfile}
     cdk deploy gfb-datalake-glue-stack --require-approval never --profile ${AwsProfile}
-    #    cdk deploy gfb-datalake-s3-stack --require-approval never --profile ${AwsProfile}
 
 fi
